3ca23e5616877462ee8956da4175d1327d1b857e
Brad Christie <bch@sitecore.net>
2018-07-25 15:28:33 -0400

    Fixed hard reference to docker hub repo.

--- Deploy.ps1
+++ Deploy.ps1
@@ -3,6 +3,6 @@ $ErrorActionPreference = "Stop"
 Write-Host "Deploy started..."
 
 $env:DOCKER_PASS | docker login --username bchsitecore --password-stdin
-docker push $env:DOCKER_USER/iis
+docker push bchsitecore/iis
 
 Write-Host "Deploy complete."
